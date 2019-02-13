a0ce350ae0429c42827dcc32a513008c787c242e
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-02-13 12:56:48 +0100

    Add promises to getGif

--- components/App.js
+++ components/App.js
@@ -21,23 +21,28 @@ App = React.createClass({
     },
 
 
-    getGif: function(searchingText, callback) { 
-        var GIPHY_API_URL = 'https://api.giphy.com';
-        var GIPHY_PUB_KEY = 'aszz0vmoVnuHJLwjg9vJUGV77ZWkINA3';
-        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  
-        var xhr = new XMLHttpRequest();  
-        xhr.open('GET', url);
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-               var data = JSON.parse(xhr.responseText).data; 
-                var gif = { 
-                    url: data.fixed_width_downsampled_url,
-                    sourceUrl: data.url
+    getGif: function (searchingText, callback) {
+        return new Promise(
+            function (resolve, reject) {
+                var GIPHY_API_URL = 'https://api.giphy.com';
+                var GIPHY_PUB_KEY = 'aszz0vmoVnuHJLwjg9vJUGV77ZWkINA3';
+                var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+                var xhr = new XMLHttpRequest();
+                xhr.onload = function () {
+                    if (xhr.status === 200) {
+                        resolve(this.response);
+                        var data = JSON.parse(xhr.responseText).data;
+                        var gif = {
+                            url: data.fixed_width_downsampled_url,
+                            sourceUrl: data.url
+                        };
+                        reject(new Error(this.statusText));
+                        callback(gif);
+                    }
                 };
-                callback(gif); 
-            }
-        };
-        xhr.send();
+                xhr.open('GET', url);
+                xhr.send();
+            });
     },
 
 
@@ -49,15 +54,29 @@ App = React.createClass({
             width: '90%'
         };
 
-        return (
-            <div style={styles}>
-                <h1>Wyszukiwarka GIFow!</h1>
-                <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
-                <Search onSearch={this.handleSearch} />
-                <Gif loading={this.state.loading}
-                    url={this.state.gif.url}
-                    sourceUrl={this.state.gif.sourceUrl} />
-            </div>
+        return ( <
+            div style = {
+                styles
+            } >
+            <
+            h1 > Wyszukiwarka GIFow! < /h1> <
+            p > Znajdź gifa na < a href = 'http://giphy.com' > giphy < /a>. Naciskaj enter, aby pobrać kolejne gify.</p >
+            <
+            Search onSearch = {
+                this.handleSearch
+            }
+            /> <
+            Gif loading = {
+                this.state.loading
+            }
+            url = {
+                this.state.gif.url
+            }
+            sourceUrl = {
+                this.state.gif.sourceUrl
+            }
+            /> <
+            /div>
         );
     }
 });
